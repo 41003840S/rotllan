aab481257fa634c5125f8b8956ad6f77536c157d
Manuel <41003840s@iespoblenou.org>
2016-04-22 19:19:45 +0200

    Antes del find

--- app/src/main/java/com/project/rotllan/MainActivity.java
+++ app/src/main/java/com/project/rotllan/MainActivity.java
@@ -13,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -24,6 +25,15 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        WebView webview = (WebView) findViewById(R.id.webView);
+
+
+        webview.loadUrl("http://slashdot.org/");
+
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
